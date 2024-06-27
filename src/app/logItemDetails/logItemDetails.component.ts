00f3c7d1e1c8fcd00cae00e240a3bae0ac8dfe50
Lewis Moten <lewis.moten@codejamboree.com>
2024-06-27 04:44:15 -0400

    show spinner when loading details

--- src/app/logItemDetails/logItemDetails.component.ts
+++ src/app/logItemDetails/logItemDetails.component.ts
@@ -1,16 +1,25 @@
 /* eslint-disable no-console */
-import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, Inject } from '@angular/core';
 import { NgFor, CommonModule } from '@angular/common';
+import { MatPaginatorModule, MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 import { LogItemDetailsService } from './logItemDetails.service';
 import { LogItemDetailData } from './LogItemDetailData';
-import { MatPaginatorModule, MatPaginator, PageEvent } from '@angular/material/paginator';
 import { highlightSearchTerms } from '../search/highlightSearchTerms';
+import { Notice } from '../Notice';
 
 @Component({
   selector: 'log-item-details',
   templateUrl: './logItemDetails.component.html',
   styleUrls: ['./logItemDetails.component.scss'],
-  imports: [MatPaginatorModule, NgFor, CommonModule],
+  imports: [
+    MatPaginatorModule,
+    NgFor,
+    CommonModule,
+    MatProgressSpinnerModule
+  ],
   standalone: true
 })
 export class LogItemDetailsComponent implements OnInit {
@@ -22,8 +31,14 @@ export class LogItemDetailsComponent implements OnInit {
   pageSize: number = 10;
   pageIndex: number = 0;
   data: LogItemDetailData[] = [];
+  waiting: boolean = false;
+  notice: Notice;
 
-  constructor(private logItemDetailsService: LogItemDetailsService) {
+  constructor(
+    private logItemDetailsService: LogItemDetailsService,
+    @Inject(MatSnackBar) private snackBar: MatSnackBar,
+  ) {
+    this.notice = new Notice(snackBar);
   }
   hasData() {
     return this.totalItems !== 0;
@@ -33,12 +48,20 @@ export class LogItemDetailsComponent implements OnInit {
   }
 
   loadData(pageIndex: number, pageSize: number) {
+    this.waiting = true;
     this.logItemDetailsService.getPage(this.id, pageIndex + 1, pageSize)
-      .subscribe(response => {
-        this.pageSize = pageSize;
-        this.pageIndex = pageIndex;
-        this.data = response.data;
-        this.totalItems = response.total;
+      .subscribe({
+        next: response => {
+          this.pageSize = pageSize;
+          this.pageIndex = pageIndex;
+          this.data = response.data;
+          this.totalItems = response.total;
+        }, error: (error: Error) => {
+          this.notice.error(error.message);
+          this.waiting = false;
+        }, complete: () => {
+          this.waiting = false;
+        }
       });
   }
   handlePageEvent(event: PageEvent) {
