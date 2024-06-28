d5801f7d55f948de693a8d32ac152143ce82958d
Lewis Moten <lewis.moten@codejamboree.com>
2024-06-28 01:44:37 -0400

    set hard boundaries for each color

--- src/app/logList/utils/generateMatrixLinearGradient.ts
+++ src/app/logList/utils/generateMatrixLinearGradient.ts
@@ -14,22 +14,31 @@ export function generateMatrixLinearGradient(
 
   let gradient = 'linear-gradient(to right';
 
+  const totalCells = horizontalCells * verticalCells;
+  const colorPercent = 1 / totalCells;
+
   const hexSize = 3; // 0 to 4095
   const hues = 360;
   const step = 360 / hues;
   let hex = hash.substring(0, hexSize);
   let i = hexSize;
+  let colorIndex = 0;
   for (let y = 0; y < verticalCells; y++) {
     for (let x = 0; x < horizontalCells; x++) {
       const value = parseInt(hex, 16);
       const hue = Math.floor((value % hues) * step);
-      gradient += `, hsl(${hue}, 100%, 50%)`;
+      const start = Math.floor(colorIndex * colorPercent * 100);
+      const end = Math.floor((colorIndex + 1) * colorPercent * 100);
+      const color = `hsl(${hue}, 100%, 50%)`;
+      gradient += `, ${color} ${start}%`;
+      gradient += `, ${color} ${end}%`;
       i += hexSize;
       hex = hash.substring(i, i + hexSize);
       if (hex.length < hexSize) {
         i = hexSize - hex.length;
         hex += hash.substring(0, i);
       }
+      colorIndex++;
     }
   }
   gradient += ")";
